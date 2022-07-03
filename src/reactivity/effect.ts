924ab786e898ec547e5677e2d748e3b005121924
dafengxiang <1205679862@qq.com>
2022-07-03 22:09:09 +0800

    feat: effect的scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,7 +1,7 @@
 
 class ReactiveEffect {
   private _fn: any
-  constructor(_fn) {
+  constructor(_fn, public scheduler?) {
     this._fn = _fn
   }
   run() {
@@ -37,13 +37,17 @@ export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
 
 let activeEffect
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
   _effect.run()
 
   return _effect.run.bind(_effect)
--- src/reactivity/testts/effect.spec.ts
+++ src/reactivity/testts/effect.spec.ts
@@ -29,4 +29,30 @@ describe('effect', () => {
     expect(foo).toBe(12)
     expect(r).toBe('foo')
   })
+
+  it('scheduler', () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    })
+    const obj = reactive({ foo: 1 })
+    // 第二个参数obj，键scheduler是一个函数
+    const runner = effect(() => {
+      dummy = obj.foo
+    },
+      { scheduler }
+    )
+    // 一开始不会执行
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    obj.foo++
+    // 值改变，调用schaduler，并不会调用第一个参数
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+
+    // 调用runner执行第一个参数fn
+    run()
+    expect(dummy).toBe(2)
+  })
 })
