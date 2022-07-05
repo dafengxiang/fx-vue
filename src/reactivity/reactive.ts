9e73362b344a2ade14b1c8be5047ea6856ca7048
dafengxiang <1205679862@qq.com>
2022-07-05 22:30:38 +0800

    feat: readonly实现

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,19 +1,33 @@
-import {track,trigger} from './effect'
+import { track, trigger } from './effect'
 export function reactive(raw) {
 
   return new Proxy(raw, {
     get(target, key) {
       const res = Reflect.get(target, key)
       // 依赖收集
-      track(target,key)
+      track(target, key)
       return res
     },
     set(target, key, value) {
       const res = Reflect.set(target, key, value)
 
       // 触发依赖
-      trigger(target,key)
+      trigger(target, key)
       return res
+    }
+  })
+}
+export function readonly(raw) {
+
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      // 依赖收集
+      // track(target, key)
+      return res
+    },
+    set(target, key, value) {
+      return true
     }
   })
 }
